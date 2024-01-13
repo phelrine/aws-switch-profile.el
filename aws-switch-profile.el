5cb1de3f894fd2842a4e4fa6eb54f00cacf66ad3
phelrine <s.ngskx615@gmail.com>
2024-01-13 16:59:11 +0900

    Refactor AWS credential management in aws-switch-profile
    - Introduce aws-switch-profile-session eieio class for structured management of AWS session credentials.
    - Add aws-switch-profile-session-setenv method to set environment variables from session objects.
    - Implement aws-switch-profile--session-credentials-cache hash table for caching session credentials.
    - Modify aws-switch-profile--set-env-vars to utilize class and cache for enhanced session handling.
    - Parse and track expiration field using parse-iso8601-time-string for credentials expiry validation.
    - Adjust aws-switch-profile--get-session-info to account for credential expiration and caching logic.

--- aws-switch-profile.el
+++ aws-switch-profile.el
@@ -9,6 +9,8 @@
 
 (require 'json)
 (require 'cl-lib)
+(require 'eieio)
+(require 'parse-time)
 
 (defgroup aws-switch-profile nil
   "Manage AWS IAM roles and environment variables in Emacs."
@@ -56,15 +58,65 @@
                       (gethash "source_profile" (cdr profile)))
                     profiles))
 
-(defun aws-switch-profile--set-env-vars (session-info)
+(defclass aws-switch-profile-session ()
+  ((access-key-id
+    :initarg :access-key-id
+    :initform ""
+    :type string
+    :documentation "access key id")
+   (secret-access-key
+    :initarg :secret-access-key
+    :initform ""
+    :type string
+    :documentation "secret key id")
+   (session-token
+    :initarg :session-token
+    :initform ""
+    :type string
+    :documentation "session token")
+   (expiration
+    :initarg :expiration
+    :initform nil
+    :type list
+    :documentation "expiration")))
+
+(cl-defmethod aws-switch-profile-session-setenv ((session aws-switch-profile-session))
+  "Set the current environment's AWS credentials based on SESSION object."
+  (setenv "AWS_ACCESS_KEY_ID" (oref session access-key-id))
+  (setenv "AWS_SECRET_ACCESS_KEY" (oref session secret-access-key))
+  (setenv "AWS_SESSION_TOKEN" (oref session session-token)))
+
+(defvar aws-switch-profile--session-credentials-cache (make-hash-table :test 'equal)
+  "Cache for storing AWS session credentials.")
+
+(defun aws-switch-profile--get-session-credentials (role-arn)
+  "Get the AWS session credentials from the cache if it exists and is not expired.
+ROLE-ARN is the role Amazon Resource Name.
+
+If the credentials are expired or don't exist, return nil."
+  (let ((session-object (gethash role-arn aws-switch-profile--session-credentials-cache)))
+    (when session-object
+      (let ((expiration-time (oref session-object expiration)))
+        ;; If expiration-time is not set (nil) or is in the future, return the credentials.
+        ;; Convert the expiration string to time and compare it to the current time.
+        (if (or (null expiration-time)
+                (time-less-p (current-time) expiration-time)
+                session-object))))))
+
+(defun aws-switch-profile--set-env-vars (role-arn session-info)
   "Set AWS environment variables based on SESSION-INFO."
   (pcase-let ((`((Credentials . ((AccessKeyId . ,access-key-id)
                                  (SecretAccessKey . ,secret-access-key)
-                                 (SessionToken . ,session-token))))
-                session-info))
-    (setenv "AWS_ACCESS_KEY_ID" access-key-id)
-    (setenv "AWS_SECRET_ACCESS_KEY" secret-access-key)
-    (setenv "AWS_SESSION_TOKEN" session-token)))
+                                 (SessionToken . ,session-token)
+                                 (Expiration . ,expiration))))
+               session-info))
+    (let ((session (aws-switch-profile-session
+                    :access-key-id access-key-id
+                    :secret-access-key secret-access-key
+                    :session-token session-token
+                    :expiration (parse-iso8601-time-string expiration))))
+      (aws-switch-profile-session-setenv session)
+      (puthash role-arn session aws-switch-profile--session-credentials-cache))))
 
 (defun aws-switch-profile--get-session-info (source-profile role-arn role-session-name duration-seconds mfa-serial mfa-code)
   "Retrieve session information for a given AWS IAM role."
@@ -84,12 +136,14 @@
                  (mapcar (lambda (key) (gethash key profile))
                          '("role_arn" "source_profile" "mfa_serial"))))
       (when role-arn
-        (let ((mfa-code (when mfa-serial (read-string "Enter MFA code: ")))
-              (role-session-name (or (gethash "role_session_name" profile) aws-switch-profile-aws-role-session-name))
-              (duration-seconds 3600))
-          (aws-switch-profile--set-env-vars
-           (aws-switch-profile--get-session-info source-profile role-arn role-session-name duration-seconds mfa-serial mfa-code))
-          (message "Switched to AWS profile: %s" profile-name))))))
+        (if-let (session (aws-switch-profile--get-session-credentials role-arn))
+            (aws-switch-profile-session-setenv session)
+          (let ((mfa-code (when mfa-serial (read-string "Enter MFA code: ")))
+                (role-session-name (or (gethash "role_session_name" profile) aws-switch-profile-aws-role-session-name))
+                (duration-seconds 3600))
+            (aws-switch-profile--set-env-vars role-arn
+                                              (aws-switch-profile--get-session-info source-profile role-arn role-session-name duration-seconds mfa-serial mfa-code))))
+        (message "Switched to AWS profile: %s" profile-name)))))
 
 ;;;###autoload
 (defun aws-switch-profile-clear-aws-session ()
@@ -99,6 +153,7 @@
   (setenv "AWS_ACCESS_KEY_ID" nil)
   (setenv "AWS_SECRET_ACCESS_KEY" nil)
   (setenv "AWS_SESSION_TOKEN" nil)
+  (setq aws-switch-profile--session-credentials-cache (make-hash-table :test 'equal))
   (message "AWS session variables cleared, reverted to default credentials."))
 
 (provide 'aws-switch-profile)
